d6b48c825a212698aef2ab79b6434bc08b3c89ae
raj <rajtony533@gmail.com>
2024-01-27 08:47:26 +0530

    added actor images

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ function App() {
       <h1>Tollywood Film Actors</h1>
       
       <h1>Megastar Chiranjeevi</h1>
+      <img src="./images/chiru.jpeg"></img>
       
-
       <h1>Akkineni Nagarjuna</h1>
+      <img src="./images/nagarjun.jpeg"></img>
       
       <h1>Nandamuri Balakrishna</h1>
+      <img src="./images/balayya.jpeg"></img>
     
     </div>
   );
