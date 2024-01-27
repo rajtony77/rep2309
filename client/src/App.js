7d60efdc55d0fcaf37a554e640405ed4799db143
raj <rajtony533@gmail.com>
2024-01-27 08:44:27 +0530

    added actor name

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>Tollywood Film Actors</h1>
+      
       <h1>Megastar Chiranjeevi</h1>
+      
+
       <h1>Akkineni Nagarjuna</h1>
+      
+      <h1>Nandamuri Balakrishna</h1>
+    
     </div>
   );
 }
